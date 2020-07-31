39505d23df97714c15f8bee6c5123919f8f321b9
1000JI <chjh1992@gmail.com>
2020-07-31 10:52:38 +0900

    feat: VegeXI Init

---
+++ VegeXI/VegeXI/Supporting/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  VegeXI
+//
+//  Created by 천지운 on 2020/07/31.
+//  Copyright © 2020 TeamSloth. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ VegeXI/VegeXI/Tutorial/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  VegeXI
+//
+//  Created by 천지운 on 2020/07/31.
+//  Copyright © 2020 TeamSloth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+}
+
